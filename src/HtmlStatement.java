d8a79f45afccd5464ccffbfd07bb5af02802fb2c
marianateatinir <marianateatinir@gmail.com>
2021-08-07 23:11:14 -0300

    Commit 17

--- src/HtmlStatement.java
+++ src/HtmlStatement.java
@@ -3,20 +3,31 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-        "</EM></H1><P>\n";
+        String result = nameText(aCustomer.getName());
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            //show figures for each rental
-            result += each.getMovie().getTitle()+ ": " +
-            String.valueOf(each.getCharge()) + "<BR>\n";
+            //show figures for this rental
+            result += rentalText(each.getMovie().getTitle(), each.getCharge());
         }
         //add footer lines
-        result += "<P>You owe <EM>" +
-        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + 
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        "</EM> frequent renter points<P>";
+        result += owedText(aCustomer.getTotalCharge());
+        result += earnedText(aCustomer.getTotalFrequentRenterPoints());
         return result;
+    }
+
+    private String nameText(String name) {
+        return "<H1>Rentals for <EM>" + name + "</EM></H1><P>\n";
+    }
+
+    private String rentalText(String title, double charge) {
+        return title + ": " + String.valueOf(charge) + "<BR>\n";
+    }
+
+    private String owedText(double value) {
+        return "<P>You owe <EM>" + String.valueOf(value) + "</EM><P>\n";
+    }
+
+    private String earnedText(int points) {
+        return "On this rental you earned <EM>" + String.valueOf(points) + "</EM> frequent renter points<P>";
     }
 }
--- src/TextStatement.java
+++ src/TextStatement.java
@@ -3,20 +3,31 @@ import java.util.Enumeration;
 public class TextStatement extends Statement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-        "\n";
+        String result = nameText(aCustomer.getName());
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(each.getCharge()) + "\n";
+            result += rentalText(each.getMovie().getTitle(), each.getCharge());
         }
         //add footer lines
-        result += "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
+        result += owedText(aCustomer.getTotalCharge());
+        result += earnedText(aCustomer.getTotalFrequentRenterPoints());
         return result;
+    }
+
+    private String nameText(String name) {
+        return "Rental Record for " + name + "\n";
+    }
+
+    private String rentalText(String title, double charge) {
+        return "\t" + title + "\t" + String.valueOf(charge) + "\n";
+    }
+
+    private String owedText(double value) {
+        return "Amount owed is " + String.valueOf(value) + "\n";
+    }
+
+    private String earnedText(int points) {
+        return "You earned " + String.valueOf(points) + " frequent renter points";
     }
 }
